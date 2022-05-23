14e08d7c540526cae8aeb1b6b6cfaf37e2ad8138
Ramchandra Bhate <93651934+ramchandra-bhate@users.noreply.github.com>
2022-05-23 15:21:05 +0530

    Update testcode.ps1

--- testcode.ps1
+++ testcode.ps1
@@ -1,10 +1,6 @@
-az login -u johncena@ramchandralaboutlook.onmicrosoft.com -p CantSeeMe@123
+az login -u $username -p $password
 
 az group create `
   --name RG01 `
   --location "South Central US"
   
-  write-output "Done creating RG"
-  
-#New-AzResourceGroup -Name RG01 -Location "South Central US" -Tag @{Type="Dev"; Department="IT"}
-
